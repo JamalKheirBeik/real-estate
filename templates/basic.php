a05290dd3701511977a17d9573e46a907ae2b45e
JamalKheirBeik <jamalkheirbeik@gmail.com>
2022-04-29 01:44:31 +0300

    ignore basic template folder

--- templates/basic.php
+++
@@ -1,8 +0,0 @@
-<?php
-$title = ''; // setting the title must be before requiring the header
-require('./includes/header.php');
-?>
-<!-- page content goes here -->
-
-<?php
-require('./includes/footer.php');
